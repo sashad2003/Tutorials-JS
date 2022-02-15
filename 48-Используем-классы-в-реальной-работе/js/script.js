32ae5140e013b306c2b503fd2be39ac7498a3451
Sashik <sashad2003@gmail.com>
2022-02-15 19:44:40 +0200

    48 Создали меню ресторана с помощью классов и метода render

---
+++ 48-Используем-классы-в-реальной-работе/js/script.js
@@ -0,0 +1,210 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Tabs
+
+    const
+        tabsParent = document.querySelector('.tabheader__items'),
+        tabs = tabsParent.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent');
+
+    function hideTabContent() {
+        tabsContent.forEach((item) => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        })
+
+        tabs.forEach((item) => {
+            item.classList.remove('tabheader__item_active');
+        })
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains('tabheader__item') && !target.classList.contains('tabheader__item_active')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
+
+
+    // Timer
+
+    const dedLine = '2022-05-07 08:36';
+
+    function getTimeRemaining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(number) {
+        if (number >= 0 && number < 10) {
+            number = `0${number}`;
+        }
+        return number;
+    }
+
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                console.log(`My son's birthday`);
+            }
+        }
+    }
+
+    setClock('.timer', dedLine);
+
+
+    // Modal
+
+    const modalButton = document.querySelectorAll('[data-modal]');
+    const modal = document.querySelector('.modal');
+    const modalCloseButton = document.querySelector('[data-close]');
+
+    function modalToggle() {
+        modal.classList.toggle('show');
+        document.body.classList.toggle('overflow-hidden');
+        clearInterval(modalTimerId);
+    }
+
+    modalButton.forEach((button) => {
+        button.addEventListener('click', modalToggle);
+    })
+
+    modalCloseButton.addEventListener('click', modalToggle);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            console.log('Modal clicked');
+            modalToggle();
+        }
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            modalToggle('Escape');
+        }
+    });
+
+
+    function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            modalToggle();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+
+    // const modalTimerId = setTimeout(modalToggle, 6000);
+
+    window.addEventListener('scroll', showModalByScroll);
+
+    // Используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 3.2;
+            this.changeToNIS();
+        }
+
+        changeToNIS() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> шек/день</div>
+                </div>
+            </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+// 1
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+
+    ).render();
+
+    // 2
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        10,
+        '.menu .container'
+
+    ).render();
+
+    // 3
+    new MenuCard(
+        'img/tabs/post.jpg',
+        "post",
+        'Меню “Постное”',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+        10,
+        '.menu .container'
+
+    ).render();
+
+});
